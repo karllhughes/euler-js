f71e160bb15ddd7d78d31619202ba44b4e694d00
Karl Hughes <khughes.me@gmail.com>
2015-12-29 23:02:25 -0500

    Solving problem 7

---
+++ problem7.js
@@ -0,0 +1,34 @@
+/*
+ * Problem 3
+ * https://projecteuler.net/problem=3
+ */
+
+"use strict";
+
+const maxCount = 10001;
+let count = 0;
+let j = 2;
+
+while(j) {
+    if (isPrime(j)) {
+        count++;
+        if (count == maxCount) {
+            console.log(j);
+            break;
+        }
+    }
+    j++;
+}
+
+function isPrime(num) {
+    for(var div = 2; div < num; div++) {
+        if(divisibleBy(num, div)) {
+            return false;
+        }
+    }
+    return num > 1;
+}
+
+function divisibleBy(num, div) {
+    return num % div === 0;
+}
