05626647768056d4663619aa2947acfefba2fcdc
Karl Hughes <khughes.me@gmail.com>
2016-01-24 13:42:28 -0500

    Migrating to shared file

--- problem2.js
+++ problem2.js
@@ -5,13 +5,15 @@
 
 "use strict";
 
+const shared = require("./shared.js");
+
 let i = 1;
 let j = 0;
 let total = 0;
 
 while (i <= 4000000) {
     let next = i + j;
-    if (divisibleBy(next, 2)) {
+    if (shared.divisibleBy(next, 2)) {
         total = total + next;
     }
     j = i;
@@ -19,7 +21,3 @@ while (i <= 4000000) {
 }
 
 console.log(total);
-
-function divisibleBy(num, div) {
-    return num % div === 0;
-}
