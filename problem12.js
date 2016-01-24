d70d375c33f85e4a6837c851f4f0941c75ab4627
Karl Hughes <khughes.me@gmail.com>
2016-01-24 15:06:38 -0500

    Solving problem 12

--- problem12.js
+++ problem12.js
@@ -21,7 +21,7 @@ function countDivisors(num) {
     let count = 0;
     for (let i = 1; i <= Math.sqrt(num); i++) {
         if (euler.divisibleBy(num, i)) {
-            count++;
+            count += 2;
         }
     }
     return count;
